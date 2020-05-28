8ce4fd8e7e8c259ef5be8ede262feb6f003fe1fb
keg504 <kevin.gnanaraj@hotmail.com>
2020-05-27 22:48:11 -0400

    fix type error

--- application.py
+++ application.py
@@ -97,9 +97,8 @@ def index():
                 price = float(stock_info[data])
                 shares = int(stock_data[i]["shares"])
                 shares_price_value = round(price*shares, 2)
-                total_value_f = float(total_value)
                 shares_price_value_f = float(shares_price_value)
-                total_value_f += shares_price_value_f
+                total_value += shares_price_value_f
                 price = "{:,.2f}".format(price)
                 shares_price = "{:,.2f}".format(shares_price_value)
                 stock_data[i].update({"shares_price":shares_price})
